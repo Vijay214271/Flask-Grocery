c2402694dcfeb947294a6f14a254e7a61ef44170
Vijay214271 <162944831+Vijay214271@users.noreply.github.com>
2024-05-19 17:34:46 +0530

    Add files via upload

--- data1.sql
+++ data1.sql
@@ -4,347 +4,197 @@
 -- ------------------------------------------------------
 -- Server version	8.0.36
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-/*!50503 SET NAMES utf8mb4 */
-;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
-;
-/*!40103 SET TIME_ZONE='+00:00' */
-;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */
-;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
-;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
-;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `additems`
 --
 
-DROP TABLE
-IF
-  EXISTS `additems`;
-  /*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-  /*!50503 SET character_set_client = utf8mb4 */
-;
-  CREATE TABLE `additems` (
-    `item_id` binary(16) NOT NULL
-    , `item_name` longtext
-    , `dis` longtext NOT NULL
-    , `qyt` int NOT NULL DEFAULT '0'
-    , `category` enum(
-      'snacks'
-      , 'meat_and_seafood'
-      , 'bakery_products'
-      , 'beverages'
-      , 'fruits'
-      , 'vegetables'
-    ) DEFAULT NULL
-    , `price` int NOT NULL
-    , `addedby` varchar(50) DEFAULT NULL
-    , `imgid` varchar(10) DEFAULT NULL
-    , PRIMARY KEY (`item_id`)
-    , KEY `addedby` (`addedby`)
-    , CONSTRAINT `additems_ibfk_1` FOREIGN KEY (`addedby`) REFERENCES `admindetails` (`admin_email`)
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-  /*!40101 SET character_set_client = @saved_cs_client */
-;
+DROP TABLE IF EXISTS `additems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `additems` (
+  `item_id` binary(16) NOT NULL,
+  `item_name` longtext,
+  `dis` longtext NOT NULL,
+  `qyt` int NOT NULL DEFAULT '0',
+  `category` enum('snacks','meat_and_seafood','bakery_products','beverages','fruits','vegetables') DEFAULT NULL,
+  `price` int NOT NULL,
+  `addedby` varchar(50) DEFAULT NULL,
+  `imgid` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `addedby` (`addedby`),
+  CONSTRAINT `additems_ibfk_1` FOREIGN KEY (`addedby`) REFERENCES `admindetails` (`admin_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-  --
-  -- Dumping data for table `additems`
-  --
+--
+-- Dumping data for table `additems`
+--
 
-  LOCK TABLES `additems` WRITE;
-  /*!40000 ALTER TABLE `additems` DISABLE KEYS */
-;
-  INSERT INTO
-    `additems`
-  VALUES
-    (
-      _binary 'E\�\�\�o\��\�!k'
-      , 'Sugar'
-      , '******'
-      , 20
-      , 'beverages'
-      , 34
-      , 'jaiannapareddy2003@gmail.com'
-      , 'T7pI3e.jpg'
-    )
-    , (
-      _binary '\�U]�\��\�!k'
-      , 'Good day'
-      , 'powered by '
-      , 22
-      , 'snacks'
-      , 20
-      , 'jaiannapareddy2003@gmail.com'
-      , 'D4xT9m.jpg'
-    );
-  /*!40000 ALTER TABLE `additems` ENABLE KEYS */
-;
-  UNLOCK TABLES;
+LOCK TABLES `additems` WRITE;
+/*!40000 ALTER TABLE `additems` DISABLE KEYS */;
+INSERT INTO `additems` VALUES (_binary 'Y\�\�\�\�U\�e�\�','Sprite 200ml','This is sprite cool drink',10,'beverages',90,'vijaypatnala147@gmail.com','H2vK7f.jpg'),(_binary 'i\�c\�\�\�U\�e�\�','carrot','good for health',1,'vegetables',45,'vijaypatnala147@gmail.com','M8bA0w.jpg'),(_binary '\�\�\�)\�e�\�','Chocolate','This is chocolate1',10,'bakery_products',100,NULL,'N6iY7j.jpg');
+/*!40000 ALTER TABLE `additems` ENABLE KEYS */;
+UNLOCK TABLES;
 
-  --
-  -- Table structure for table `admindetails`
-  --
+--
+-- Table structure for table `admindetails`
+--
 
-  DROP TABLE
-  IF
-    EXISTS `admindetails`;
-    /*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-    /*!50503 SET character_set_client = utf8mb4 */
-;
-    CREATE TABLE `admindetails` (
-      `admin_id` varchar(6) NOT NULL
-      , `admin_name` varchar(20) NOT NULL
-      , `admin_email` varchar(50) NOT NULL
-      , `admin_mobile` bigint NOT NULL
-      , `password` varchar(8) DEFAULT NULL
-      , PRIMARY KEY (`admin_email`)
-      , UNIQUE KEY `admin_id` (`admin_id`)
-      , UNIQUE KEY `admin_mobile` (`admin_mobile`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-    /*!40101 SET character_set_client = @saved_cs_client */
-;
+DROP TABLE IF EXISTS `admindetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admindetails` (
+  `admin_id` varchar(6) NOT NULL,
+  `admin_name` varchar(20) NOT NULL,
+  `admin_email` varchar(50) NOT NULL,
+  `admin_mobile` bigint NOT NULL,
+  `password` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`admin_email`),
+  UNIQUE KEY `admin_id` (`admin_id`),
+  UNIQUE KEY `admin_mobile` (`admin_mobile`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-    --
-    -- Dumping data for table `admindetails`
-    --
+--
+-- Dumping data for table `admindetails`
+--
 
-    LOCK TABLES `admindetails` WRITE;
-    /*!40000 ALTER TABLE `admindetails` DISABLE KEYS */
-;
-    INSERT INTO
-      `admindetails`
-    VALUES
-      (
-        'R5aR9w'
-        , 'Jaya Vardhan'
-        , 'jaiannapareddy2003@gmail.com'
-        , 7386484900
-        , '321'
-      );
-    /*!40000 ALTER TABLE `admindetails` ENABLE KEYS */
-;
-    UNLOCK TABLES;
+LOCK TABLES `admindetails` WRITE;
+/*!40000 ALTER TABLE `admindetails` DISABLE KEYS */;
+INSERT INTO `admindetails` VALUES ('A5fB4f','vijay','vijaypatnala147@gmail.com',9059730554,'1234');
+/*!40000 ALTER TABLE `admindetails` ENABLE KEYS */;
+UNLOCK TABLES;
 
-    --
-    -- Table structure for table `contactus`
-    --
+--
+-- Table structure for table `contactus`
+--
 
-    DROP TABLE
-    IF
-      EXISTS `contactus`;
-      /*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-      /*!50503 SET character_set_client = utf8mb4 */
-;
-      CREATE TABLE `contactus` (
-        `name` varchar(30) NOT NULL
-        , `emailid` varchar(40) NOT NULL
-        , `message` tinytext NOT NULL
-      ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-      /*!40101 SET character_set_client = @saved_cs_client */
-;
+DROP TABLE IF EXISTS `contactus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contactus` (
+  `name` varchar(30) NOT NULL,
+  `emailid` varchar(40) NOT NULL,
+  `message` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-      --
-      -- Dumping data for table `contactus`
-      --
+--
+-- Dumping data for table `contactus`
+--
 
-      LOCK TABLES `contactus` WRITE;
-      /*!40000 ALTER TABLE `contactus` DISABLE KEYS */
-;
-      INSERT INTO
-        `contactus`
-      VALUES
-        (
-          'Jaya Vardhan Annapareddy'
-          , 'jaiannapareddy2003@gmail.com'
-          , 'non'
-        )
-        , (
-          'Jaya Vardhan Annapareddy'
-          , 'jaiannapareddy2003@gmail.com'
-          , ': )  :)\r\n'
-        );
-      /*!40000 ALTER TABLE `contactus` ENABLE KEYS */
-;
-      UNLOCK TABLES;
+LOCK TABLES `contactus` WRITE;
+/*!40000 ALTER TABLE `contactus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contactus` ENABLE KEYS */;
+UNLOCK TABLES;
 
-      --
-      -- Table structure for table `orders`
-      --
+--
+-- Table structure for table `orders`
+--
 
-      DROP TABLE
-      IF
-        EXISTS `orders`;
-        /*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-        /*!50503 SET character_set_client = utf8mb4 */
-;
-        CREATE TABLE `orders` (
-          `ordid` bigint NOT NULL AUTO_INCREMENT
-          , `itemid` binary(16) DEFAULT NULL
-          , `item_name` longtext
-          , `qyt` int DEFAULT NULL
-          , `total_price` bigint DEFAULT NULL
-          , `user` varchar(25) DEFAULT NULL
-          , PRIMARY KEY (`ordid`)
-          , KEY `itemid` (`itemid`)
-          , KEY `user` (`user`)
-          , CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`itemid`) REFERENCES `additems` (`item_id`)
-          , CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user`) REFERENCES `users` (`user_name`)
-        ) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-        /*!40101 SET character_set_client = @saved_cs_client */
-;
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `ordid` bigint NOT NULL AUTO_INCREMENT,
+  `itemid` binary(16) DEFAULT NULL,
+  `item_name` longtext,
+  `qyt` int DEFAULT NULL,
+  `total_price` bigint DEFAULT NULL,
+  `user` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`ordid`),
+  KEY `itemid` (`itemid`),
+  KEY `user` (`user`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`itemid`) REFERENCES `additems` (`item_id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user`) REFERENCES `users` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-        --
-        -- Dumping data for table `orders`
-        --
+--
+-- Dumping data for table `orders`
+--
 
-        LOCK TABLES `orders` WRITE;
-        /*!40000 ALTER TABLE `orders` DISABLE KEYS */
-;
-        INSERT INTO
-          `orders`
-        VALUES
-          (
-            2
-            , _binary 'E\�\�\�o\��\�!k'
-            , 'Sugar'
-            , 1
-            , 34
-            , 'Jaya Vardhan Annapareddy'
-          )
-          , (
-            3
-            , _binary 'E\�\�\�o\��\�!k'
-            , 'Sugar'
-            , 1
-            , 34
-            , 'Jaya Vardhan Annapareddy'
-          );
-        /*!40000 ALTER TABLE `orders` ENABLE KEYS */
-;
-        UNLOCK TABLES;
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
-        --
-        -- Table structure for table `reviews`
-        --
+--
+-- Table structure for table `reviews`
+--
 
-        DROP TABLE
-        IF
-          EXISTS `reviews`;
-          /*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-          /*!50503 SET character_set_client = utf8mb4 */
-;
-          CREATE TABLE `reviews` (
-            `itemid` binary(16) NOT NULL
-            , `user` varchar(25) NOT NULL
-            , `title` tinytext
-            , `review` text
-            , `rating` int DEFAULT NULL
-            , `date` datetime DEFAULT CURRENT_TIMESTAMP
-            , PRIMARY KEY (`itemid`, `user`)
-          ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-          /*!40101 SET character_set_client = @saved_cs_client */
-;
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reviews` (
+  `itemid` binary(16) NOT NULL,
+  `user` varchar(25) NOT NULL,
+  `title` tinytext,
+  `review` text,
+  `rating` int DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`itemid`,`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-          --
-          -- Dumping data for table `reviews`
-          --
+--
+-- Dumping data for table `reviews`
+--
 
-          LOCK TABLES `reviews` WRITE;
-          /*!40000 ALTER TABLE `reviews` DISABLE KEYS */
-;
-          /*!40000 ALTER TABLE `reviews` ENABLE KEYS */
-;
-          UNLOCK TABLES;
+LOCK TABLES `reviews` WRITE;
+/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+UNLOCK TABLES;
 
-          --
-          -- Table structure for table `users`
-          --
+--
+-- Table structure for table `users`
+--
 
-          DROP TABLE
-          IF
-            EXISTS `users`;
-            /*!40101 SET @saved_cs_client     = @@character_set_client */
-;
-            /*!50503 SET character_set_client = utf8mb4 */
-;
-            CREATE TABLE `users` (
-              `user_id` binary(16) DEFAULT NULL
-              , `user_name` varchar(25) NOT NULL
-              , `u_mobile` bigint DEFAULT NULL
-              , `email` varchar(255) NOT NULL
-              , `gender` enum('M', 'F', 'NAN') DEFAULT NULL
-              , `address` varchar(256) DEFAULT NULL
-              , `password` varchar(20) DEFAULT NULL
-              , PRIMARY KEY (`user_name`)
-              , UNIQUE KEY `email` (`email`)
-              , UNIQUE KEY `unique_email` (`email`)
-            ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-            /*!40101 SET character_set_client = @saved_cs_client */
-;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` binary(16) DEFAULT NULL,
+  `user_name` varchar(25) NOT NULL,
+  `u_mobile` bigint DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` enum('M','F','NAN') DEFAULT NULL,
+  `address` varchar(256) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`user_name`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `unique_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-            --
-            -- Dumping data for table `users`
-            --
+--
+-- Dumping data for table `users`
+--
 
-            LOCK TABLES `users` WRITE;
-            /*!40000 ALTER TABLE `users` DISABLE KEYS */
-;
-            INSERT INTO
-              `users`
-            VALUES
-              (
-                _binary '\�\�c\�l\��\�!k'
-                , 'Jaya Vardhan Annapareddy'
-                , 7386484900
-                , 'jaiannapareddy2003@gmail.com'
-                , 'M'
-                , 'Vijayawada '
-                , '111'
-              )
-              , (
-                _binary '�u�S\�HP{�]1\�'
-                , 'nagu'
-                , 6645845436
-                , 'nagalakshmi@codegnan.com'
-                , 'F'
-                , 'vijayawada'
-                , '123'
-              );
-            /*!40000 ALTER TABLE `users` ENABLE KEYS */
-;
-            UNLOCK TABLES;
-            /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
-;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (_binary '�u�S\�HP{�]1\�','nagu',6645845436,'nagalakshmi@codegnan.com','F','vijayawada','123'),(_binary 'Ԉf�\�r\�e�\�','vijay',9059730554,'vijaypatnala147@gmail.com','M','vijayawada','1111');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-            /*!40101 SET SQL_MODE=@OLD_SQL_MODE */
-;
-            /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
-;
-            /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
-;
-            /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-            /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-            /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
-            /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
-;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-            -- Dump completed on 2024-05-16 17:16:27
+-- Dump completed on 2024-05-18 21:26:40
